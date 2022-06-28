6a09b717c7554c084bc0ea9c027bae54e423f214
unknown <formacio@BARCELO-5KQVJSF.formacio.local>
2022-06-28 11:00:03 +0200

    Exercici-3 Tema-3 completad.

---
+++ Sprint_1/Tema_3/Nivell_1/Exercici_3/titol.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+		<title>Tema 3 Exercici 1</title>
+		<meta charset="UTF-8">
+	</head>
+
+	<body>
+
+<?php 
+$strVar  = "Lluís";
+
+echo "format: h1."; echo "<br>";
+
+echo "<h1>";
+echo $strVar;
+echo "</h1>"
+
+?>
+
+	</body>
+
+</html>
+
