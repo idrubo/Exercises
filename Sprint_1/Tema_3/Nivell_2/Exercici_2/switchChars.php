6bab6b0a729fbe910af9e800ba56aa4ba0cf87e4
unknown <formacio@BARCELO-5KQVJSF.formacio.local>
2022-06-28 19:53:52 +0200

    Exercise-2 Level-2 Topic-3 fisnished.

---
+++ Sprint_1/Tema_3/Nivell_2/Exercici_2/switchChars.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+		<title>Exercici 1</title>
+		<meta charset="UTF-8">
+	</head>
+
+<?php
+
+function switchChars ($str)
+{
+	$strArr = str_split ($str, 1);
+
+	$first = $strArr [0];
+	$iLast = strlen ($str) - 1;
+
+	$strArr [0] = $strArr [$iLast];
+	$strArr [$iLast] = $first;
+
+	return implode ("", $strArr);
+}
+
+$str1 = "xyz";
+echo "Initial string: ";
+echo $str1 . "<br>"; 
+
+echo "Result string: ";
+echo switchChars ($str1); echo "<br><br>";
+
+$str1 = "a";
+echo "Initial string: ";
+echo $str1 . "<br>"; 
+
+echo "Result string: ";
+echo switchChars ($str1); echo "<br><br>";
+
+$str1 = "ab";
+echo "Initial string: ";
+echo $str1 . "<br>"; 
+
+echo "Result string: ";
+echo switchChars ($str1); echo "<br><br>";
+
+$str1 = "xyzt";
+echo "Initial string: ";
+echo $str1 . "<br>"; 
+
+echo "Result string: ";
+echo switchChars ($str1); echo "<br><br>";
+
+?>
+
+	<body>
+
+	</body>
+
+</html>
+
