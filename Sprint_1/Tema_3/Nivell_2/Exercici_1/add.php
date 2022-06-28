b61cdeef6d30a5874b0409ceffcda451df2230ae
unknown <formacio@BARCELO-5KQVJSF.formacio.local>
2022-06-28 13:10:28 +0200

    Exercise-1 Level-2 fisnished.

---
+++ Sprint_1/Tema_3/Nivell_2/Exercici_1/add.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+		<title>Tema m Exercici n</title>
+		<meta charset="UTF-8">
+	</head>
+
+	<body>
+
+<?php 
+
+function add ($X, $Y)
+{
+	if ($X == $Y) return 2 * ($X + $Y);
+
+	return $X + $Y;
+}
+
+$a = 1;
+$b = 2;
+
+echo "Entrada: " . $a . ", " . $b; echo "<br>";
+echo "Salida: " . $Y = add (1, 2); echo "<br><br>";
+
+echo "Entrada: " . $a . ", " . $b; echo "<br>";
+echo "Salida: " . $Y = add (3, 2); echo "<br><br>";
+
+echo "Entrada: " . $a . ", " . $b; echo "<br>";
+echo "Salida: " . $Y = add (2, 2); echo "<br><br>";
+
+?>
+
+	</body>
+
+</html>
+
